b959d59329548191cdf31729e2c2cb21d91bc9d4
ragnyll <ragnyll@gallowzhumour.dev>
2021-11-08 23:10:29 -0600

    finally have query params working

--- api/src/bin/app.rs
+++ api/src/bin/app.rs
@@ -8,9 +8,7 @@ use std::str::FromStr;
 use mongodb::{Client, options::ClientOptions};
 use mongodb::bson::{doc};
 use futures::stream::TryStreamExt;
-use warp::{
-    http::{header, Method},
-};
+use std::collections::HashMap;
 
 const DEFAULT_IP: &str = "127.0.0.1";
 const DEFAULT_PORT: u16 = 3000;
@@ -24,29 +22,14 @@ struct Doc {
     tags: Vec<String>,
 }
 
-struct Offset {
-    start: u64,
-}
-
-impl FromStr for Offset {
-    type Err = std::num::ParseIntError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Offset {
-            start: s.parse::<u64>()?,
-        })
-    }
-}
-
 #[tokio::main]
 async fn main() {
     let addr = build_ip_addr(DEFAULT_IP, DEFAULT_PORT);
 
-    let offset_header = warp::header::<Offset>("64");
-
     // Register Routes
-    let routes = warp::path!("docs")
-        .and(warp::get())
+    let routes = warp::get()
+        .and(warp::path("docs"))
+        .and(warp::query::<HashMap<String, String>>())
         .and_then(list_all_doc_tags);
 
     println!("==== Starting webserver on {}", addr);
@@ -59,17 +42,18 @@ fn build_ip_addr(ip: &str, port: u16) -> SocketAddr {
     SocketAddr::new(ip, port)
 }
 
-async fn list_all_doc_tags() -> Result<impl warp::Reply, std::convert::Infallible> {
-    let client_options = ClientOptions::parse(DB_HOST).await.unwrap();
-    let client = Client::with_options(client_options).unwrap();
-    let db = client.database("tagdb");
-    let collection = db.collection::<Doc>("docs");
-    let mut cursor = collection.find(None, None).await.unwrap();
+async fn list_all_doc_tags(param: HashMap<String, String>) -> Result<impl warp::Reply, std::convert::Infallible> {
+        let client_options = ClientOptions::parse(DB_HOST).await.unwrap();
+        let client = Client::with_options(client_options).unwrap();
+        let db = client.database("tagdb");
+        let collection = db.collection::<Doc>("docs");
+        let mut cursor = collection.find(None, None).await.unwrap();
 
-    let mut docs = vec![];
-    // Iterate over the results of the cursor.
-    while let Some(my_doc) = cursor.try_next().await.unwrap() {
-        docs.push(my_doc);
-    }
+        let mut docs = vec![];
+        // Iterate over the results of the cursor.
+        while let Some(my_doc) = cursor.try_next().await.unwrap() {
+            docs.push(my_doc);
+        }
+    println!("{:?}", param);
     Ok(warp::reply::json(&docs))
 }
